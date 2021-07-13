7e9ba7ff5778b9882877b30a992441ec81473dac
chaosJS <lichao779@sina.com>
2021-07-13 20:16:16 +0800

    update micro regist

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import Vue from 'vue';
+import { registerMicroApps, start } from 'qiankun';
 import App from './App.vue';
 import router from './router';
 import store from './store';
@@ -10,6 +11,23 @@ Vue.use(Icon);
 Vue.prototype.$message = message;
 Vue.config.productionTip = false;
 
+// 注册子应用
+const mircoApps = [
+	{
+		name: 'jianshu',
+		entry: 'http://localhost:9000',
+		container: '#frame',
+		activeRule: '/react',
+	},
+	{
+		name: 'vueApp',
+		entry: 'http://localhost:9002',
+		container: '#frame',
+		activeRule: '/vue',
+	},
+];
+registerMicroApps(mircoApps);
+start();
 new Vue({
 	router,
 	store,
